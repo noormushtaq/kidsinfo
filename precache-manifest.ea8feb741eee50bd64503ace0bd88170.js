e0ff2b8c12b1ca081757955e80174b7723f6db25
noormushtaq <noormushtaq1985@gmail.com>
2021-02-07 13:57:12 -0600

    Updates

---
+++ precache-manifest.ea8feb741eee50bd64503ace0bd88170.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882187bc9574ca3f29b855e11ce4f1cb",
+    "url": "/kidsinfo/index.html"
+  },
+  {
+    "revision": "d8322b9e09df04fc3446",
+    "url": "/kidsinfo/static/css/main.ac170510.chunk.css"
+  },
+  {
+    "revision": "d6217f5f3ffc1b8cef65",
+    "url": "/kidsinfo/static/js/2.8c2859e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kidsinfo/static/js/2.8c2859e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8322b9e09df04fc3446",
+    "url": "/kidsinfo/static/js/main.e6717296.chunk.js"
+  },
+  {
+    "revision": "7187dd84764ca6827c36",
+    "url": "/kidsinfo/static/js/runtime-main.ccce3f88.js"
+  },
+  {
+    "revision": "186f3031e5a0b59b01153b27d21b8f8a",
+    "url": "/kidsinfo/static/media/8.186f3031.png"
+  },
+  {
+    "revision": "28098f207d83d1146b09e1d2a3d513dc",
+    "url": "/kidsinfo/static/media/Antibiotic.28098f20.jpg"
+  },
+  {
+    "revision": "1a61c8f1af911bf61cb5e04c966cd9ea",
+    "url": "/kidsinfo/static/media/Asteroid.1a61c8f1.jpg"
+  },
+  {
+    "revision": "b79ecb7936e71ea8a5f0ad4d47bf65f5",
+    "url": "/kidsinfo/static/media/Peninsula.b79ecb79.png"
+  },
+  {
+    "revision": "c0d0d39e6e04e3dadc6d1c54ffecb08a",
+    "url": "/kidsinfo/static/media/algae.c0d0d39e.png"
+  },
+  {
+    "revision": "69144c7dcc1d09828372608e1afffd92",
+    "url": "/kidsinfo/static/media/animal.69144c7d.png"
+  },
+  {
+    "revision": "e72c0cd4a1bee61239e0696fc1b4238e",
+    "url": "/kidsinfo/static/media/atmosphere.e72c0cd4.png"
+  },
+  {
+    "revision": "c89d3e95a6cad1f226619bdba56e8201",
+    "url": "/kidsinfo/static/media/back.c89d3e95.png"
+  },
+  {
+    "revision": "a7a73fe854d8f90ac5702160b7ba90af",
+    "url": "/kidsinfo/static/media/berry.a7a73fe8.png"
+  },
+  {
+    "revision": "020994223feb3ab4416f265e06752538",
+    "url": "/kidsinfo/static/media/chlorophyll.02099422.png"
+  },
+  {
+    "revision": "d26df4ea47f4b440d83b8750079fb7ee",
+    "url": "/kidsinfo/static/media/citrus.d26df4ea.png"
+  },
+  {
+    "revision": "78e2f54df422900a18aa2bb67b6b5ae1",
+    "url": "/kidsinfo/static/media/conifer.78e2f54d.png"
+  },
+  {
+    "revision": "58b19d0a1d206ec620f735addb734dc7",
+    "url": "/kidsinfo/static/media/continent.58b19d0a.png"
+  },
+  {
+    "revision": "5ed6b9337131ef7ae6a277bc8a1198c3",
+    "url": "/kidsinfo/static/media/countries.5ed6b933.png"
+  },
+  {
+    "revision": "6c55f78b367a401e2601708dbd83ec6f",
+    "url": "/kidsinfo/static/media/drought.6c55f78b.jpg"
+  },
+  {
+    "revision": "d9e65c237c51cc1db399a6e2e94e7981",
+    "url": "/kidsinfo/static/media/earth.d9e65c23.png"
+  },
+  {
+    "revision": "a27128ca72384aa90fd0e0037840a277",
+    "url": "/kidsinfo/static/media/fern.a27128ca.png"
+  },
+  {
+    "revision": "899be5fc093b369fdcffa470afec1e42",
+    "url": "/kidsinfo/static/media/flood.899be5fc.png"
+  },
+  {
+    "revision": "d9e2cfb0856f9e42ab782c8182c823c6",
+    "url": "/kidsinfo/static/media/flower.d9e2cfb0.png"
+  },
+  {
+    "revision": "a4349b371b51e7554e23823203b6d092",
+    "url": "/kidsinfo/static/media/flower2.a4349b37.png"
+  },
+  {
+    "revision": "9c46dfa34ba16641d67e7f0df502c32c",
+    "url": "/kidsinfo/static/media/fruit.9c46dfa3.png"
+  },
+  {
+    "revision": "6a35e15857ffd054a465ef85e12366b6",
+    "url": "/kidsinfo/static/media/galaxy.6a35e158.png"
+  },
+  {
+    "revision": "22d1a1be89245c10d5cb3b38489b572b",
+    "url": "/kidsinfo/static/media/greenArrow.22d1a1be.jpg"
+  },
+  {
+    "revision": "69302afd07cc3e41082b1545db9b78f0",
+    "url": "/kidsinfo/static/media/heatWave.69302afd.jpg"
+  },
+  {
+    "revision": "78732ededa4d45ab89af43a3bea9e9e0",
+    "url": "/kidsinfo/static/media/island.78732ede.jpg"
+  },
+  {
+    "revision": "d5f4e7a67c2e75e3984731914c780995",
+    "url": "/kidsinfo/static/media/leaf.d5f4e7a6.png"
+  },
+  {
+    "revision": "55f6f19d23e7ca1606371f14bd974e4c",
+    "url": "/kidsinfo/static/media/letterSound.55f6f19d.mp3"
+  },
+  {
+    "revision": "7402c38d61b06c041760e84b242d3704",
+    "url": "/kidsinfo/static/media/magma.7402c38d.jpg"
+  },
+  {
+    "revision": "34cca42f706adfe64cef100329f0743c",
+    "url": "/kidsinfo/static/media/moss.34cca42f.png"
+  },
+  {
+    "revision": "e34483f372ca5801599950594a39cac3",
+    "url": "/kidsinfo/static/media/nectar.e34483f3.png"
+  },
+  {
+    "revision": "c5aca7215e5f5c78391e27465334b21d",
+    "url": "/kidsinfo/static/media/next.c5aca721.png"
+  },
+  {
+    "revision": "44ba357be2c0940a4f0a0c713121abae",
+    "url": "/kidsinfo/static/media/northPole.44ba357b.jpg"
+  },
+  {
+    "revision": "8e81507194ee0714d701e5cf124e55f8",
+    "url": "/kidsinfo/static/media/ocean.8e815071.jpg"
+  },
+  {
+    "revision": "9d3f5af4485604facb564ef94e774f46",
+    "url": "/kidsinfo/static/media/packIce.9d3f5af4.jpg"
+  },
+  {
+    "revision": "6cba2de43dc276d65a20bbabfa23ee7b",
+    "url": "/kidsinfo/static/media/pip.6cba2de4.png"
+  },
+  {
+    "revision": "a5e2fcd067c4ce6324ef47918dd7f42f",
+    "url": "/kidsinfo/static/media/pit.a5e2fcd0.png"
+  },
+  {
+    "revision": "87bfb5e6761e6a34caf20d3e7696469d",
+    "url": "/kidsinfo/static/media/planisphere.87bfb5e6.png"
+  },
+  {
+    "revision": "0efd6f12b66646a8e2813b685ddf2882",
+    "url": "/kidsinfo/static/media/plant.0efd6f12.png"
+  },
+  {
+    "revision": "cc804cd98e3cdd23c1db44e394f299e6",
+    "url": "/kidsinfo/static/media/pollination.cc804cd9.png"
+  },
+  {
+    "revision": "e64b7ec3c339294d7a2f3677033a5740",
+    "url": "/kidsinfo/static/media/root.e64b7ec3.png"
+  },
+  {
+    "revision": "628cd8047506ac103c1070f38ae1c536",
+    "url": "/kidsinfo/static/media/sap.628cd804.png"
+  },
+  {
+    "revision": "b54bd9ececeb3e476b80168b2bcc213d",
+    "url": "/kidsinfo/static/media/seed.b54bd9ec.png"
+  },
+  {
+    "revision": "10038a1ef9165f8f93442c1eb60ac104",
+    "url": "/kidsinfo/static/media/sorry.10038a1e.png"
+  },
+  {
+    "revision": "2359e14c142b31ea3471b3f2aaac61a6",
+    "url": "/kidsinfo/static/media/star.2359e14c.jpg"
+  },
+  {
+    "revision": "cec3d80fd0ff0c965602f4fe3499745f",
+    "url": "/kidsinfo/static/media/tornado.cec3d80f.jpg"
+  },
+  {
+    "revision": "a85de2a41f1d9c3d499c375ec8a57063",
+    "url": "/kidsinfo/static/media/tsunami.a85de2a4.jpg"
+  },
+  {
+    "revision": "19ef0683986377007704bf4987e56c6f",
+    "url": "/kidsinfo/static/media/valley.19ef0683.jpg"
+  },
+  {
+    "revision": "da3d16e07f4737d6901e7a8b207313d9",
+    "url": "/kidsinfo/static/media/vein.da3d16e0.png"
+  }
+]);
